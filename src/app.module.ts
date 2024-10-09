6d94027c828369de83c0a0aee1be9f082137de14
JoseMauricioC <josemauriciocamacho1@gmail.com>
2024-10-09 10:48:44 -0300

    segundo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonasModule } from './personas/personas.module';
+import { EstudiantesModule } from './estudiantes/estudiantes.module';
+import { InterpretesModule } from './interpretes/interpretes.module';
 
 @Module({
-  imports: [PersonasModule],
+  imports: [PersonasModule, EstudiantesModule, InterpretesModule],
   controllers: [AppController],
   providers: [AppService],
 })
