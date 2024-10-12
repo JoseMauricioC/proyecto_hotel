9d7233bcbf0b3cad662d5cd8b91a0b33cbde328d
JoseMauricioC <josemauriciocamacho1@gmail.com>
2024-10-12 09:17:16 -0300

    tercero

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PersonasModule } from './personas/personas.module';
 import { EstudiantesModule } from './estudiantes/estudiantes.module';
 import { InterpretesModule } from './interpretes/interpretes.module';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
-  imports: [PersonasModule, EstudiantesModule, InterpretesModule],
+  imports: [PersonasModule, EstudiantesModule, InterpretesModule, ClientesModule],
   controllers: [AppController],
   providers: [AppService],
 })
